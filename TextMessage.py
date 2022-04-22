e553eafad11af5b1c66385b5f7a69baacd745434
OwenDurham <104180324+OwenDurham@users.noreply.github.com>
2022-04-21 22:15:02 -0500

    Update TextMessage.py

--- TextMessage.py
+++ TextMessage.py
@@ -1,18 +1,18 @@
 import os
 from twilio.rest import Client
 import padas as pd
-from datetime import date
+from datetime import datetime
 
 df = pd.read_csv (r'/Users/owendurham/Downloads/Interview-Challenge-main\addressBook.csv')
 
 account_sid = os.environ['ACa781c9ef3e203041b6949c9d347433ef']
-auth_token = os.environ['ef7956361f0bd473389b050fcfd8158d']
-client = Client(account_sid, auth_token)
+client = Client(account_sid)
 
-today = date.today()
+current_month = datetime.now().month
 
 for x in df.iloc['Date of Birth']:
-  if x == today:
+  month = x.split('/')
+  if month[0] == current_month:
     message = client.messages.create(
       body='Happy Birthday ' df.iloc['First Name'].values[x] 'from Owen Durham! Call me at 515-681-1414 to plan a lunch sometime.',
       from_='515-325-4937',
